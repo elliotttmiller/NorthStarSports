60f8df4d532d25e412dee555c2882b4a6706d96d
unknown <elliotttmiller@hotmail.com>
2025-09-17 23:33:29 -0500

    Functioning - added mobile external connection

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,6 +1,9 @@
-export { HomePage } from './HomePage'
-export { GamePage } from './GamePage'
-export { GameDetailPage } from './GameDetailPage'
-export { MyBetsPage } from './MyBetsPage'
-export { AccountPage } from './AccountPage'
-export { OtherPage } from './OtherPage'
+// This file should only export pages for routing. Do not use as an entry point.
+// If you want to run the app, use main.tsx
+
+export { default as AccountPage } from "./AccountPage";
+export { default as GameDetailPage } from "./GameDetailPage";
+export { default as GamePage } from "./GamePage";
+export { default as HomePage } from "./HomePage";
+export { default as MyBetsPage } from "./MyBetsPage";
+export { default as OtherPage } from "./OtherPage";
